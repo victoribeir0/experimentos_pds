b3871b5e46ccf7193e54d74633938b3d8bba1e8b
Victor Ribeiro <victoribeiro@outlook.com.br>
2021-04-05 15:54:14 -0300

    Create eco.m

---
+++ experimento_01/eco.m
@@ -0,0 +1,32 @@
+#{
+    Eco com filtro comb.
+    x = Sinal de entrada.
+    d = Atraso (em amstras).
+    t = Tempo de decaímento (em amostras).
+    f = Feedback, controla o quanto do sinal atrasado é incluído na efeito.
+    y = Sinal de saída com eco.    
+#}
+
+function y = eco(x,d,t,f)
+    
+    d = d/1000;  # Atrasos em ms.
+    d = round(d*16000); # Converte para amostras.
+    t = t/1000;
+    t = round(t*16000);    
+    
+    N = length(x);    # Dimensão do sinal.
+    y = zeros(1,N+t); # Sinal de saida com eco.
+    
+    # Sinal x de entrada 'crescido', para ficar do mesmo tamanho da saída e evitar problemas no laço for.
+    xx = zeros(1,N+t); 
+    xx(1:N) = x; 
+    
+    # A parte inicial do sinal de saída é igual a entrada, o eco começa a partir de d.
+    y(1:d) = x(1:d); 
+    
+    # Filtro comb, indo até o número de amostras de atraso.
+    for n = d:N+t               
+        y(n) = xx(n) + f*y(n-(d-1));
+    endfor
+
+endfunction
